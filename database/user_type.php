5283a735f7844a3020d9fbd4153f9ddc87c0a0a6
Telma <up201806586@edu.fe.up.pt>
2023-01-25 19:19:59 +0000

    some functions DB

---
+++ database/user_type.php
@@ -0,0 +1,14 @@
+<?PHP
+
+    function getUserTypebyID($id){ //retorna cliente, gestor ou tecnico. query confirmada no site da DB 
+        global $conn;
+		$query = "	SELECT  user_type.type			    As   type
+					FROM user_type INNER JOIN users
+					ON users.type=user_type.id
+					WHERE users.id='".$id."'
+				";
+		$result = pg_exec($conn, $query);
+		return $result;
+    }
+
+?>
