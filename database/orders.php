3ed33752c4f2f3650b79ea01bc928475f595227c
Telma <up201806586@edu.fe.up.pt>
2023-01-26 19:30:02 +0000

    orders visual studio passou se

--- database/orders.php
+++ database/orders.php
@@ -1,21 +1,31 @@
 <?PHP
-    function getStateofOrder(){
+    function getStateofOrder($idOrder){
+        global $conn;
+		$query = "	SELECT  orders.state 			As   state, 
+					FROM    orders
+					WHERE   orders.state='".$idOrder."'
+				";
+		$result = pg_exec($conn, $query);
+		return $result;
+    }
+
+    function getAllOrdersofUserbyID($idUser){
 
     }
 
-    function updateStateofOrder(){
+    function updateStateofOrder($idOrder){
         
     }
 
-    function getPricesofOrder(){
+    function getPricesofOrder($idOrder){
 
     }
 
-    function setPriceofOrder(){
+    function setPriceofOrder($idOrder){
 
     }
 
-    function createOrder(){
+    function createOrder($idOrder){
 
     }
 
