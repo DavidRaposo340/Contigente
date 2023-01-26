d78512fb41c6c9b3391f7bf19ff4602522703867
Telma <up201806586@edu.fe.up.pt>
2023-01-26 12:26:05 +0000

    new functions

--- database/products.php
+++ database/products.php
@@ -1,28 +1,79 @@
 <?PHP
-
 	function getAllProducts($familia, $no_gluten, $no_lact, $vegan, $prec_min, $prec_max ){ //query confirmada na DB
-		//retorna vetor/lista com: img_path, nome, id e price de todos os produtos (conforme os filtros) 
-		global $conn;
-		$query = "	SELECT  products.id 			As   id, 
-							products.family_id 		As   familyid, 
-							products.quantity		As   quantity, 
-							products.price   		As   price 
-							products.image_name 	AS 	 img_path,
-							products.name 			AS 	 nome 
-					FROM products INNER JOIN family_products
-					ON products.family_id=family_products.id
-					WHERE family_products.name='".$familia."'
-					AND products.price BETWEEN $prec_min AND $prec_max;
-				";
+        //retorna vetor/lista com: img_path, nome, id e price de todos os produtos (conforme os filtros) 
+        global $conn;
 
+        $query = "    SELECT  products.id             As   id, 
+                            products.family_id         As   familyid, 
+                            products.quantity        As   quantity, 
+                            products.price           As   price,
+                            products.image_name     AS      img_path,
+                            products.name             AS      nome 
+                            FROM products INNER JOIN family_products
+                            ON products.family_id=family_products.id
+                            WHERE family_products.name='".$familia."'
+                            AND products.price BETWEEN $prec_min AND $prec_max;
+                    ";
+
+        $result = pg_exec($conn, $query);
+
+        return $result;
+    }
+
+	/*function getAllProducts($familia, $no_gluten, $no_lact, $vegan, $prec_min, $prec_max ){ //query confirmada na DB
+		//retorna vetor/lista com: img_path, nome, id e price de todos os produtos (conforme os filtros) 
+		//falta fazer os filtros como no exemplo do Cities
+		global $conn;
+		$query = "    SELECT  	products.id             As   id, 
+									products.family_id      As   familyid, 
+									products.quantity       As   quantity, 
+									products.price          As   price,
+									products.image_name     AS   img_path,
+									products.name           AS   nome 
+									FROM products INNER JOIN family_products
+									ON products.family_id=family_products.id
+									WHERE family_products.name='".$familia."'
+									AND products.price BETWEEN $prec_min AND $prec_max;
+			";
+		if($familia){ //se a familia nao for nula, faz os filtros pela familia
+			$query .= "AND family_products.name='".$familia."'";
+		}				
+		else{ 
+
+		}
 		$result = pg_exec($conn, $query);
-		echo "DEBUG num_rows: " . pg_num_rows($result);
-		echo $result;
+		return $result;
+	}*/
+
+
+	//FUNÇAO PROFESSOR GET ALL CITIES
+	/* 
+	function getAllCities($city, $countries){
+		global $conn;
+		$query = "select * from cities where 1=1";
+		if($city)	{
+			$query .= " AND name like '$city'";
+		}
+
+		if(!empty($countries) && sizeof($countries) > 0){
+			$query .= " AND ";
+
+			for($i=0; $i < sizeof($countries);$i++){
+					if($i>0){
+					$query .= " OR ";
+					}
+					$query .= "country = '". $countries[$i]."'";
+			}
+		}
+		$query .= " order by country;";
+		$result = pg_exec($conn, $query);
 		return $result;
 	}
+	*/
 
-	function getProductByID($id){ //query confirmada na DB
-		//retorna img_path, nome, id, price, familia e restr (e descr) do produto 
+
+	//Retorna img_path, nome, id, price, familia e restr (e descr) do produto 
+	function getProductByID($id){ 
 		global $conn;
 		$query = "	SELECT  products.id 			As   id, 
 							products.family_id 		As   familiaid,
@@ -40,7 +91,7 @@
 	}
 
 
-	function updateProductPrice($id, $newprice){ //da erro na DB
+	function updateProductPrice($id, $newprice){ 
 		global $conn;
 
 		$updateQuery = "UPDATE products
@@ -51,7 +102,14 @@
 
 	}
 
-	function getQuantityofProductbyID($id){ //funcionou na DB
+	function getPriceofProduct($id){
+		$query = "	SELECT  products.price   As   price, 
+						FROM products 
+						WHERE products.id='".$id."'
+				";
+	}
+
+	function getQuantityofProductbyID($id){ 
 		global $conn;
 
 		$query = "SELECT products.quantity as quant
@@ -86,6 +144,8 @@
 		$result = pg_exec($conn, $query);
 		return $query;	
 	}
+
+	
 
 	
 ?>
--- database/users.php
+++ database/users.php
@@ -5,11 +5,9 @@
 		global $conn;
 
 		$insertQuery = "INSERT INTO users (type, name, address, email, password, no_gluten, no_lacti, vegan)
-						VALUES ('3','".$nome."','".$address."','".$email."','".$encrypt_pass."','".$no_gluten."', '".$no_lact."', '".$vegan."')"; 
-			
+						VALUES ('3','".$nome."','".$address."','".$email."','".$encrypt_pass."','".$no_gluten."', '".$no_lact."', '".$vegan."');"; 	
 		$result = pg_exec($conn, $insertQuery);
-		return $result;
-	}
+		}
 
 	function getUserByEmailAndPass($email, $pass){ //se existir dou o id, se nao existir NULL
 		global $conn;
@@ -20,7 +18,24 @@
 					";
 
 		$result = pg_exec($conn, $query);
-		return $result;
+		$row = pg_fetch_row($result);
+		return $row[0];
 	}
 
+	function emailExists($email){ //retorna id do user caso o email exista, se nao existir retorna NULL
+		global $conn;
+		$query = "	SELECT users.id AS id
+					FROM users
+					WHERE users.email='".$email."'
+					";
+		
+		$result = pg_exec($conn, $query);
+		if($result==NULL){
+			return NULL;
+		}
+		else{
+			$row = pg_fetch_row($result);
+			return $row[0];
+		}
+	}
 ?>
