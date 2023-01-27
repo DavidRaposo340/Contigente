cfd2e01640e6ecaf58fa4e6f57cd4666387587a4
David <up201806281@fe.up.pt>
2023-01-27 16:59:54 +0000

    fix: db queries insert, remove uni ao cart; fix: frutas e vegi bug

--- acoes/cliente/action_remove_all_un_carrinho.php
+++ acoes/cliente/action_remove_all_un_carrinho.php
@@ -7,5 +7,6 @@
 
     $id = $_GET['id'];
 
+    removeLinefromShoppingCart($_SESSION['user'], $id);
     header("Location: ".$path2root."paginas_form/cliente/listar_carrinho.php");
 ?>
--- paginas_form/cliente/listar_carrinho.php
+++ paginas_form/cliente/listar_carrinho.php
@@ -28,7 +28,7 @@
     ?>
     
     <div class="flex-box-encomendas">
-        <h2> Lista de Encomendas: </h2>
+        <h2> Carrinho de compras: </h2>
         <div class="table_style">
             <table>
                 <tr>
@@ -43,14 +43,14 @@
                     while (isset($row['id'])) {
 
                         echo "<tr>";
-                        echo "<td> <a href=\"".$path2root."paginas_form/produto/listar_produto_info.php?id=".$row['prod']."\"> ".$row['prod']."</td>";
+                        echo "<td> <a href=\"".$path2root."paginas_form/produto/listar_produto_info.php?id=".$row['id_prod']."\"> ".$row['name']."</td>";
                         echo "<td>".$row['quant']." Unidade(s) </td>";
-                        $total_price=getTotalPriceProductbyQuantity($row['prod'], $row['quant']); 
+                        $total_price=getTotalPriceProductbyQuantity($row['id_prod'], $row['quant']); 
                         echo "<td> ".$total_price." € </td>";
                         //echo "<td>".$row['total_price']."</td>";
-                        echo "<td> <a href=\"".$path2root."acoes/cliente/action_add1un_carrinho.php?id=".$row['prod']."\"> + 1 unidade </td>";
-                        echo "<td> <a href=\"".$path2root."acoes/cliente/action_remove1un_carrinho.php?id=".$row['prod']."\"> - 1 unidade </td>";
-                        echo "<td> <a href=\"".$path2root."acoes/cliente/action_remove_all_un_carrinho.php?id=".$row['prod']."\"> Remover tudo </td>";
+                        echo "<td> <a href=\"".$path2root."acoes/cliente/action_add1un_carrinho.php?id=".$row['id_prod']."\"> + 1 unidade </td>";
+                        echo "<td> <a href=\"".$path2root."acoes/cliente/action_remove1un_carrinho.php?id=".$row['id_prod']."\"> - 1 unidade </td>";
+                        echo "<td> <a href=\"".$path2root."acoes/cliente/action_remove_all_un_carrinho.php?id=".$row['id_prod']."\"> Remover tudo </td>";
                         echo "</tr>";
                                 
                         $row = pg_fetch_assoc($list_carrinho);
