3da5c213f89c9c9a93a000447132f8f613c17cfb
David <up201806281@fe.up.pt>
2023-01-29 16:53:25 +0000

    feature: action edit conta

--- paginas_form/cliente/form_editar_conta.php
+++ paginas_form/cliente/form_editar_conta.php
@@ -23,16 +23,27 @@
     $path2root = "../../";
     include("../../includes/navbar.php");
 
+    $user_info=getUserbyID($_SESSION['user']);
+    $_SESSION['password'] = NULL;
+    $_SESSION['conf_pass'] = NULL;
+
     if (!empty($_SESSION['nome']))              $nome = $_SESSION['nome'];
-    else $nome = "";
+    else $nome = $user_info['name'];
     if (!empty($_SESSION['address']))         $address = $_SESSION['address'];
-    else $address = "";
+    else $address = $user_info['address'];
     if (!empty($_SESSION['email']))         $email = $_SESSION['email'];
-    else $email = "";
+    else $email = $user_info['email'];
     if (!empty($_SESSION['password']))         $password = $_SESSION['password'];
     else $password = "";
     if (!empty($_SESSION['conf_pass']))     $conf_pass = $_SESSION['conf_pass'];
     else $conf_pass = "";
+
+    if ($user_info['no_gluten']=="t")     $no_gluten =1;
+    else $no_gluten = 0;
+    if ($user_info['no_lact']=="t")     $no_lact =1;
+    else $no_lact = 0;
+    if ($user_info['vegan']=="t")     $vegan =1;
+    else $vegan = 0;
     ?>
 
     <div class="form-editar">
@@ -46,14 +57,26 @@
             <p><label for="conf_pass"> Confirmar Password:</label> <input type="password" name="conf_pass" value="<?php echo $conf_pass; ?>" /> </p>
 
             <div class="form-editar-checkbox">
-                <input type="checkbox" id="gluten" name="gluten" value="1">
+                <input type="checkbox" id="gluten" name="gluten" value="1" <?php echo ($no_gluten==1 ? 'checked' : '');?>>
                 <label for="gluten"> Intoletante ao gluten</label><br>
 
-                <input type="checkbox" id="lactose" name="lactose" value="1">
+                <input type="checkbox" id="lactose" name="lactose" value="1" <?php echo ($no_lact==1 ? 'checked' : '');?>>
                 <label for="lactose"> Intoletante ao lactose</label><br>
 
-                <input type="checkbox" id="vegan" name="vegan" value="1">
+                <input type="checkbox" id="vegan" name="vegan" value="1" <?php echo ($vegan==1 ? 'checked' : '');?>>
                 <label for="vegan"> Vegan</label><br><br>
+            </div>
+
+            
+            <div class="error_msg-form">
+                <?php
+                //Se houver uma msg de erro na variável de sessão, apresenta-a e depois limpa a variável
+
+                if (!empty($_SESSION['msgErro'])) {
+                    echo "<p style=\"color:red\">" . $_SESSION['msgErro'] . "</p>";
+                    $_SESSION['msgErro'] = NULL;
+                }
+                ?>
             </div>
 
             <p><input name="checkbox_confirmar" type="submit" value="Confirmar" /> </p>
